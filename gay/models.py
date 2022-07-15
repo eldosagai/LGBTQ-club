ff83af3f677940ee29f5241b0d1c7042b28acfc7
eldosagai <105664519+eldosagai@users.noreply.github.com>
2022-07-15 19:01:16 +0600

    little bit models

--- gay/models.py
+++ gay/models.py
@@ -1,3 +1,12 @@
+import email
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    name = models.CharField(max_length=100,blank=True)
+    surname = models.CharField(max_length=100,blank=True)
+    email = models.CharField(max_length=100,blank=True)
+
+    def __str__(self):
+        return self.username
--- gay/views.py
+++ gay/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def gayshouse(request):
     return render(request,'main.html')
+
+def log_in(request):
+    return render(request,'log_in.html')
