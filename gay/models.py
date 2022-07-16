ef7fae8260c763cc27b1032367277bf2e21f4660
eldosagai <105664519+eldosagai@users.noreply.github.com>
2022-07-16 20:58:00 +0600

    something does not work that i do not know

--- gay/models.py
+++ gay/models.py
@@ -1,12 +1,11 @@
-import email
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
-class User(models.Model):
-    username = models.CharField(max_length=100)
+class User(AbstractUser):
     name = models.CharField(max_length=100,blank=True)
     surname = models.CharField(max_length=100,blank=True)
-    email = models.CharField(max_length=100,blank=True)
+    email = models.CharField(max_length=100,unique=True)
+ 
 
     def __str__(self):
         return self.username
--- gay/views.py
+++ gay/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from unicodedata import name
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .models import User
 
 # Create your views here.
 def gayshouse(request):
@@ -8,4 +11,33 @@ def log_in(request):
     return render(request,'log_in.html')
 
 def sign_up(request):
+    if request.method == "POST":
+        data=request.POST # d stands for data perv
+        email = data['email']
+        username = data['username']
+        name = data['name']
+        surname = data['surname']
+        password = data['password']
+        User.objects.create_user(
+            username = username, password = password, email = email,
+            name = name, surname = surname)
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect (request,'log_in')
     return render(request,'sign_up.html')
+    # def log_in(request):
+    # form = Log_inForm()
+    # print(request.method)
+    # if request.method == "POST":
+    #     form = Log_inForm(request.POST)
+    #     if not form.is_valid():
+    #         username = request.POST['username']
+    #         password = request.POST['password']
+    #         user = authenticate(username=username, password=password)
+    #         if user is not None:
+    #             login(request, user)
+    #             return render (request, "home.html")
+    #         else:
+    #             return render (request, "log_in.html", {"form": form, "error": "Wrong username or password"})
+    # return render(request, "log_in.html", {"form": form})
+
